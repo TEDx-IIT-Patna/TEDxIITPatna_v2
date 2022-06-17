a9d48635f8ea608ade4af903264573bd832dea43
StrixROX <40167899+StrixROX@users.noreply.github.com>
2022-06-17 06:05:53 +0530

    Footer added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <template>
   <Nav />
   <div class="home"></div>
+  <Footer />
 </template>
 
 <script>
 // @ is an alias to /src
 import Nav from '@/components/Nav.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    Nav
+    Nav,
+    Footer,
   }
 }
 </script>
