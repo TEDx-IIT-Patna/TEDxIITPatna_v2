cb46bc0dbc8a14d1125dbf5477aa50da58406796
Shilpa <skcodes29@gmail.com>
2022-06-21 20:29:08 +0530

    coming soon pages issues resolved

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -17,6 +17,17 @@ export default {
   components:
   {
     
+},
+mounted() {
+    if (localStorage.getItem('reloaded')) {
+        // The page was just reloaded. Clear the value from local storage
+        // so that it will reload the next time this page is visited.
+        localStorage.removeItem('reloaded');
+    } else {
+        // Set a flag so that we know not to reload the page twice.
+        localStorage.setItem('reloaded', '1');
+        location.reload();
+    }
 }
 }
 </script>
