609cb30253cbfac41bb7e7d79f65088d4f80952d
StrixROX <40167899+StrixROX@users.noreply.github.com>
2022-06-25 09:27:37 +0530

    Completed mobile view of past events scroller

--- src/views/PastEventsView.vue
+++ src/views/PastEventsView.vue
@@ -1,30 +1,59 @@
 <template>
-  <Nav open_button_color="white" />
-  <BackButton />
-  <section class="wrapper full">
+  <Nav :open_button_color="this.showWebView ? 'white' : 'black'" />
+  <BackButton v-if="this.showWebView" />
+
+  <section class="wrapper full" v-if="this.showWebView">
     <img src="@/assets/images/tedxiitpatna_logo-2B.png" class="logo watermark" v-if="screenWidth > 1300">
     <div class="paintedx"></div>
     <div class="pastevents-wrapper">
       <div class="title">Past Events.</div>
       <div class="event-scroller">
-        <router-link to="/pastevents/2021" class="event">
+        <div class="event">
           <div class="thumbnail">
-            <img src="@/assets/images/poster-square-2021.png">
+            <router-link to="/pastevents/2021"><img src="@/assets/images/poster-square-2021.png"></router-link>
           </div>
           <div class="year">2021</div>
-        </router-link>
-        <router-link to="/pastevents/2019" class="event">
+        </div>
+        <div class="event">
           <div class="thumbnail">
-            <img src="@/assets/images/poster-square-2019.png">
+            <router-link to="/pastevents/2019"><img src="@/assets/images/poster-square-2019.png"></router-link>
           </div>
           <div class="year">2019</div>
-        </router-link>
-        <router-link to="/pastevents/2016" class="event">
+        </div>
+        <div class="event">
           <div class="thumbnail">
-            <img src="@/assets/images/poster-square-2016.png">
+            <router-link to="/pastevents/2016"><img src="@/assets/images/poster-square-2016.png"></router-link>
           </div>
           <div class="year">2016</div>
-        </router-link>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="wrapper mobile full" v-else>
+    <img src="@/assets/images/tedxiitpatna_logo-2B.png" class="logo watermark">
+    <img src="@/assets/images/Flower-cropped.png" class="flower" style="filter: invert(100%)">
+
+    <div class="pastevents-wrapper">
+      <div class="title"><span style="color: white">P</span>ast Events.</div>
+      <div class="event-scroller">
+        <div class="event">
+          <div class="thumbnail">
+            <router-link to="/pastevents/2021"><img src="@/assets/images/poster-square-2021.png"></router-link>
+          </div>
+          <div class="year">2021</div>
+        </div>
+        <div class="event">
+          <div class="thumbnail">
+            <router-link to="/pastevents/2019"><img src="@/assets/images/poster-square-2019.png"></router-link>
+          </div>
+          <div class="year">2019</div>
+        </div>
+        <div class="event">
+          <div class="thumbnail">
+            <router-link to="/pastevents/2016"><img src="@/assets/images/poster-square-2016.png"></router-link>
+          </div>
+          <div class="year">2016</div>
+        </div>
       </div>
     </div>
   </section>
@@ -43,11 +72,13 @@ export default {
   data() {
     return {
       screenWidth: window.innerWidth,
+      showWebView: window.innerWidth >= 830,
     }
   },
   methods: {
     onResize() {
       this.screenWidth = window.innerWidth
+      this.showWebView = window.innerWidth >= 830
     },
   },
   mounted() {
@@ -63,4 +94,5 @@ export default {
 
 <style scoped>
 @import '@/assets/css/past_events.css';
+@import '@/assets/css/past_events.mobile.css';
 </style>
